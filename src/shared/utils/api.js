ee7ae625e0545747fabfc27552f1f088e352b295
Fantony <Fantony@ca-usa.com>
2025-11-06 23:08:20 -0800

    env push

--- src/shared/utils/api.js
+++ src/shared/utils/api.js
@@ -8,7 +8,9 @@ const axiosApi = axios.create({
 
 axiosApi.interceptors.request.use(
   (config) => {
-    const token = sessionStorage.getItem("auth_token");
+    // const token = sessionStorage.getItem("auth_token");
+    const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiUmFteWEgTXVuaXJhdGhuYW0iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJybXVuaXJhdGhuYW1AY2EtdXNhLmNvbSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiOTEiLCJuYmYiOiIxNzYyNDkzNjE1IiwiZXhwIjoiMTc2MjU4MDAxNSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1hc3RlciBBZG1pbiIsIlBlcm1pc3Npb24iOlsiVXNlciBNYW5hZ2VtZW50IiwiRnVsbCBBY2Nlc3MiLCJXcml0ZSBBY2Nlc3MiLCJSZWFkIE9ubHkgQWNjZXNzIiwiUmVwb3J0cyIsIkRvY3VtZW50IFVwbG9hZCIsIkNyZWF0ZSBMb2FuIiwiRWRpdCBMb2FuIElEIiwiRGVsZXRlIExvYW4iXX0.ke81ZzIi4F7a6sg0lgbas8oyazc3dzscZuS_UoI_Vu4`;
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -48,9 +50,14 @@ const handleGetApi = async (apiName, params = {}) => {
 };
 
 // POST request
-const handlePostApi = async (apiName, data = {}) => {
+const handlePostApi = async (apiName, data = {}, contentType = 'application/json') => {
   try {
-    const response = await axiosApi.post(apiName, data);
+    const config = {
+      headers: {
+        'Content-Type': contentType,
+      },
+    };
+    const response = await axiosApi.post(apiName, data, config);
     return response.data;
   } catch (error) {
     console.error(`POST ${apiName} failed:`, error);
